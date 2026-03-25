88755ca6e38f75c5a3b08450e3149135f79a2c7b
Terence Montecillo <4hoursleft@gmail.com>
2026-03-25 21:32:26 +0800

    Devblog Updated / Disable Smooth Scroll

--- scripts/load.js
+++ scripts/load.js
@@ -22,16 +22,16 @@ window.addEventListener("load", () => {
     }, 2000);
 });
 
-// initialize Lenis smooth scroll
-const lenis = new Lenis({
-    duration: 1.5,        // how long the scroll glide lasts
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),  // ease out feel
-    smooth: true,
-});
+// [DISABLED]
+// const lenis = new Lenis({
+//     duration: 1.5,        // how long the scroll glide lasts
+//     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),  // ease out feel
+//     smooth: false,
+// });
 
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-}
+// function raf(time) {
+//     lenis.raf(time);
+//     requestAnimationFrame(raf);
+// }
 
-requestAnimationFrame(raf);
+// requestAnimationFrame(raf);
